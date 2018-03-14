9f97d1c480924f23807f96a2a1a51ac4ee804c9e
Matas Petrikas <matas.petrikas@gmail.com>
2018-03-14 10:56:29 +0100

    Project and dependencies set up

--- RxHome.podspec
+++ RxHome.podspec
@@ -19,7 +19,7 @@ Pod::Spec.new do |s|
 
   s.requires_arc = true
 
-  s.source_files = 'Source/*.swift'
+  s.source_files = 'RxHome/**/*.{h,swift}'
   s.frameworks   = 'HomeKit'
   s.dependency 'RxSwift', '~> 4.0'
 end
---
+++ RxHome/RxHome/RxHome.h
@@ -0,0 +1,19 @@
+//
+//  RxHome.h
+//  RxHome
+//
+//  Created by Matas Petrikas on 14.03.18.
+//  Copyright © 2018 Matas Petrikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxHome.
+FOUNDATION_EXPORT double RxHomeVersionNumber;
+
+//! Project version string for RxHome.
+FOUNDATION_EXPORT const unsigned char RxHomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxHome/PublicHeader.h>
+
+
